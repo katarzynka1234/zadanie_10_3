dc5b1a3b06c27b5fa72374207d0b6ffc383e8433
Katarzyna Trześniewska <katarzynka.f@gmail.com>
2018-04-18 11:15:04 +0200

    Add interval and install plugin owl

--- js/script.js
+++ js/script.js
@@ -1,7 +1,57 @@
-$(function () {
-    var carouselList = $('#carousel ul');
+// $(function () {
+//     var carouselList = $('#carousel ul');
 
-    //setInterval(changeSlide, 3000);
+//     var time;
+//     startNewInterval();
+
+//     function changeSlide() {
+//         carouselList.animate({ 'marginLeft': -400 }, 500, moveFirstSlide);
+//     };
+//     function changeSlideRight () {
+//         carouselList.animate({'marginLeft': 0 }, 500, moveLastSlide);
+//     };
+
+//     function moveFirstSlide() {
+//         stopInterval();
+//         var firstItem = carouselList.find('li:first');
+//         var lastItem = carouselList.find('li:last');
+//         lastItem.after(firstItem);
+//         carouselList.css({ marginLeft: 0 });
+//         startNewInterval();
+//     };
+
+//     function moveLastSlide() {
+//         stopInterval();
+//         var firstItem = carouselList.find('li:first');
+//         var lastItem = carouselList.find('li:last');
+//         firstItem.before(lastItem);
+//         carouselList.css({ marginLeft: -400});
+//         startNewInterval();
+//     }; 
+    
+//     function startNewInterval () {
+//         time = setInterval(changeSlide, 3000); 
+//     }
+
+//     function stopInterval () {
+//         clearInterval(time);
+//     }
+
+//     $('#js-leftButton').click(function () { changeSlide() });
+//     $('#js-rightButton').click(function() { changeSlideRight() }); 
+
+// //DO wtyczki jquery Slick 
+//     //$('#your-class').slick();  
+
+// });
+
+$(document).ready(function(){
+
+        var carouselList = $('#carousel ul');
+
+    var time;
+    startNewInterval();
+
     function changeSlide() {
         carouselList.animate({ 'marginLeft': -400 }, 500, moveFirstSlide);
     };
@@ -10,27 +60,49 @@ $(function () {
     };
 
     function moveFirstSlide() {
+        stopInterval();
         var firstItem = carouselList.find('li:first');
         var lastItem = carouselList.find('li:last');
         lastItem.after(firstItem);
         carouselList.css({ marginLeft: 0 });
+        startNewInterval();
     };
 
     function moveLastSlide() {
+        stopInterval();
         var firstItem = carouselList.find('li:first');
         var lastItem = carouselList.find('li:last');
         firstItem.before(lastItem);
         carouselList.css({ marginLeft: -400});
-    };    
+        startNewInterval();
+    }; 
+    
+    function startNewInterval () {
+        time = setInterval(changeSlide, 3000); 
+    }
+
+    function stopInterval () {
+        clearInterval(time);
+    }
 
     $('#js-leftButton').click(function () { changeSlide() });
-    $('#js-rightButton').click(function() { changeSlideRight() }); 
-
-//DO wtyczki jquery Slick 
-    //$('#your-class').slick();
-
-  
-
-});
+    $('#js-rightButton').click(function() { changeSlideRight() });
 
 
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
+})
